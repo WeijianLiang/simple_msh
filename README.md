804943cdee1a3786b655a52523ced1e798f3fc78
liang <liangwj36@gmail.com>
2020-04-02 10:52:41 +0800

    add readme and mesh

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-## simple_msh
-Generate simple mesh used for cb-geo mpm
+# Simple mesh
+Generate simple mesh used for [CB-Geo MPM](https://cb-geo.github.io/mpm)
+
+Current support:
+* 3D MPM mesh (E3DH8)
+* 3D GIMP mesh (E3DH64G)
+
+2D mesh is under construction.
+
+## Node index
+The numbering of created node follow:
+* xmin -> xmax
+* ymin -> ymax
+* zmin -> zmax
+
+![Node index](order/node_id.png)
+
+## Element node order
+### MPM (ED3H8)
+
+```
+    # Hexahedron:             Hexahedron20:          Hexahedron27:
+    #        v
+    # 3----------2            3----13----2           3----13----2
+    # |\     ^   |\           |\         |\          |\         |\
+    # | \    |   | \          | 15       | 14        |15    24  | 14
+    # |  \   |   |  \         9  \       11 \        9  \ 20    11 \
+    # |   7------+---6        |   7----19+---6       |   7----19+---6
+    # |   |  +-- |-- | -> u   |   |      |   |       |22 |  26  | 23|
+    # 0---+---\--1   |        0---+-8----1   |       0---+-8----1   |
+    #  \  |    \  \  |         \  17      \  18       \ 17    25 \  18
+    #   \ |     \  \ |         10 |        12|        10 |  21    12|
+    #    \|      w  \|           \|         \|          \|         \|
+    #     4----------5            4----16----5           4----16----5
+```
+
+### GIMP (ED3H64G)
+
+![Node index](order/elem_node_order.png)
+
+## Usage 
+Just run mesh.py
+
+For gimp mesh, we can also use the code (gimp_mesh) written by Christopher Wilkes, Cambridge University. 
+> Note: gimp_mesh follow different node index scheme
--- mesh.py
+++ mesh.py
@@ -1,16 +1,7 @@
 import numpy
 
 
-def cube_mesh(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, sizex=1, sizey=1, sizez=1):
-    """Canonical tetrahedrization of the cube.
-    Input:
-    Edge lenghts of the cube
-    Number of nodes along the edges.
-    """
-    # # Generate suitable ranges for parametrization
-    # x_range = numpy.linspace(xmin, xmax, nx)
-    # y_range = numpy.linspace(ymin, ymax, ny)
-    # z_range = numpy.linspace(zmin, zmax, nz)
+def mpm_mesh(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, sizex=1, sizey=1, sizez=1):
 
     # Generate suitable ranges for parametrization
     x_range = numpy.arange(xmin, xmax+1.e-15, sizex)
@@ -21,13 +12,8 @@ def cube_mesh(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, sizex=
     ny=y_range.shape[0]
     nz=z_range.shape[0]
 
-
     # Create the vertices.
     x, y, z = numpy.meshgrid(x_range, y_range, z_range, indexing="ij")
-    # Alternative with slightly different order:
-    # ```
-    # nodes = numpy.stack([x, y, z]).T.reshape(-1, 3)
-    # ```
     nodes = numpy.array([x, y, z]).T.reshape(-1, 3)
     
     # Hexahedron:             Hexahedron20:          Hexahedron27:
@@ -64,10 +50,9 @@ def cube_mesh(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, sizex=
  
 
 
-def cube_mesh_gimp(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, sizex=1, sizey=1, sizez=1):
+def gimp_mesh(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, sizex=1, sizey=1, sizez=1):
  
-
-    # Creat additional node for gimp
+    # Creat additional node for gimp, before start and after end
     x_range = numpy.arange(xmin-sizex, xmax+sizex+1.e-15, sizex)
     y_range = numpy.arange(ymin-sizex, ymax+sizey+1.e-15, sizey)
     z_range = numpy.arange(zmin-sizez, zmax+sizez+1.e-15, sizez)
@@ -76,35 +61,35 @@ def cube_mesh_gimp(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, s
     ny=y_range.shape[0]
     nz=z_range.shape[0]
 
-
     # Create the vertices.
     x, y, z = numpy.meshgrid(x_range, y_range, z_range, indexing="ij")
     nodes = numpy.array([x, y, z]).T.reshape(-1, 3)
 
-    
+    # Get the index of the start point of each group
     temp_v0 = numpy.add.outer(numpy.array(range(1,nx-2)), nx * numpy.array(range(2,ny-1)))
     v0 = numpy.add.outer(temp_v0, nx * ny * numpy.array(range(1,nz-2))).T.flatten()
-    print v0
+
     temp_v1 = numpy.add.outer(numpy.array(range(0,nx-3)), nx * numpy.array(range(3,ny)))
     v1 = numpy.add.outer(temp_v1, nx * ny * numpy.array(range(0,nz-3))).T.flatten()
-    print v1
+
     temp_v2 = numpy.add.outer(numpy.array(range(0,nx-3)), nx * numpy.array(range(2,ny-1)))
     v2 = numpy.add.outer(temp_v2, nx * ny * numpy.array(range(0,nz-3))).T.flatten()
-    print v2
+
     temp_v3 = numpy.add.outer(numpy.array(range(0,nx-3)), nx * numpy.array(range(1,ny-2)))
     v3 = numpy.add.outer(temp_v3, nx * ny * numpy.array(range(0,nz-3))).T.flatten() 
-    print v3
+
     temp_v4 = numpy.add.outer(numpy.array(range(0,nx-3)), nx * numpy.array(range(0,ny-3)))
     v4 = numpy.add.outer(temp_v4, nx * ny * numpy.array(range(0,nz-3))).T.flatten() 
-    print v4
+ 
 
-  
+    # Initialize each goup with start point id
     group0 = numpy.repeat([v0],8,axis=0)
     group1 = numpy.repeat([v1],16,axis=0)
     group2 = numpy.repeat([v2],12,axis=0)
     group3 = numpy.repeat([v3],12,axis=0)
     group4 = numpy.repeat([v4],16,axis=0)
     
+    # Concatenate them
     elem=numpy.concatenate((group0,group1,group2,group3,group4), axis=0).transpose()
 
     #First group
@@ -180,9 +165,10 @@ def cube_mesh_gimp(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, s
     elem[:,62] += 3*nx*ny+2
     elem[:,63] += 3*nx*ny+3
 
-
     return nodes, elem
 
+#Save nodes coordinate up to 5 dicimal digit
+#Save elem node
 def save_mesh(nodes, mesh, name):
     mesh_file = name+".txt"
     f_m = open(mesh_file, "w")
@@ -190,14 +176,13 @@ def save_mesh(nodes, mesh, name):
     head = '#! elementShape hexahedron\n#! elementNumPoints {}\n{} {} \n'.format(
                                  elem.shape[1], nodes.shape[0], elem.shape[0])
     f_m.write(head)
-    numpy.savetxt(f_m, nodes, fmt="%.3f", delimiter="\t")
+    numpy.savetxt(f_m, nodes, fmt="%.5f", delimiter="\t")
     numpy.savetxt(f_m, elem, fmt="%i", delimiter="\t")
     f_m.close()
 
 
 
 def cube_particle(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, size=1.):
-
 
     # Generate suitable ranges for parametrization
     x_range = numpy.arange(xmin+0.5*size, xmax, size)
@@ -211,28 +196,25 @@ def cube_particle(xmin=0.0, xmax=1.0, ymin=0.0, ymax=1.0, zmin=0.0, zmax=1.0, si
     return particles
 
 
+#Save particle coordinate up to 5 dicimal digit
+def save_particle(particles, name):
+    file_name = name+".txt"
+    f = open(file_name, "w")
+    head = '{}\n'.format(particles.shape[0])
+    f.write(head)
+    numpy.savetxt(f, particles, fmt="%.5f", delimiter="\t")
+    f.close()
 
 
 
 
-
-
-#nodes, elem = cube_mesh(xmin=0.0, xmax=10.0, ymin=0.0, ymax=10.0, zmin=0.0, zmax=10.0, sizex=0.5, sizey=0.5, sizez=0.5)
-
-
-nodes,elem = cube_mesh_gimp(xmin=1.0, xmax=9.0, ymin=1.0, ymax=9.0, zmin=1.0, zmax=9.0, sizex=1, sizey=1, sizez=1)
+# creat and save mesh
+nodes,elem = gimp_mesh(xmin=1.0, xmax=9.0, ymin=1.0, ymax=9.0, zmin=1.0, zmax=9.0, sizex=1, sizey=1, sizez=1)
 save_mesh(nodes, elem, "gimp_mesh")
 
+# find boundary node set
 print numpy.where(nodes[:,2]<1.01)
 
-
-
-# particles=cube_particle(xmin=3, xmax=7, ymin=3, ymax=7, zmin=6, zmax=9, size=0.25)
-# particle_file = "particles.txt"
-# f_p = open(particle_file, "w")
-# head = '{}\n'.format(particles.shape[0])
-# f_p.write(head)
-# numpy.savetxt(f_p, particles, fmt="%.7f", delimiter="\t")
-# f_p.close()
-# print "number", particles.shape[0]
-# print particles
+# creat and save particle
+particles=cube_particle(xmin=3, xmax=7, ymin=3, ymax=7, zmin=6, zmax=9, size=0.25)
+save_particle(particles, "particles")
